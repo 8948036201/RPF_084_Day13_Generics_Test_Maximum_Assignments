a3cda3b96f74d300fc3e84c2ef989ba043c3325e
8948036201 <azharuddin.ansari108@gmail.com>
2021-12-14 18:37:16 +0530

    Adding Day13 Generic Test Maximum programs

---
+++ src/com/bridgelabz/genericstestmaximum/MaxDouble.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.genericstestmaximum;
+
+public class MaxDouble {
+	public Double FindMaxNumber(Double inputOne, Double inputTwo, Double inputThree) {
+		if (inputOne.compareTo(inputTwo) > 0 && inputOne.compareTo(inputThree) > 0) {
+			return inputOne;
+		} else if (inputTwo.compareTo(inputOne) > 0 && inputTwo.compareTo(inputThree) > 0) {
+			return inputTwo;
+		} else {
+			return inputThree;
+		}
+	}
+}
---
+++ src/com/bridgelabz/genericstestmaximum/MaxInteger.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.genericstestmaximum;
+
+public class MaxInteger {
+	public int FindMaxNumber(Integer inputOne, Integer inputTwo, Integer inputThree) {
+
+		if (inputOne.compareTo(inputTwo) > 0 && inputOne.compareTo(inputThree) > 0) {
+			return inputOne;
+		} else if (inputTwo.compareTo(inputOne) > 0 && inputTwo.compareTo(inputThree) > 0) {
+			return inputTwo;
+		} else {
+			return inputThree;
+		}
+	}
+}
